7005113bdd4d4723f5f39503ae4a3ce527285574
Alessio <alessiobifulco24@gmail.com>
2024-10-23 16:43:44 +0200

    add: modified queries and connected with model

--- java/app/src/main/java/model/DBModel.java
+++ java/app/src/main/java/model/DBModel.java
@@ -1,33 +1,65 @@
 package model;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.util.Objects;
+import data.Queries;
 
-import data.DAOException;
-import data.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DBModel implements Model {
-
     private final Connection connection;
 
     public DBModel(Connection connection) {
-        Objects.requireNonNull(connection, "Model created with null connection");
         this.connection = connection;
     }
 
     @Override
     public void updateOdM() {
-        // Esempio: visualizzazione dei GM nel sistema
-        try (
-            var statement = DAOUtils.prepare(connection, "SELECT * FROM GM");
-            var resultSet = statement.executeQuery();
-        ) {
-            while (resultSet.next()) {
-                System.out.println("GM: " + resultSet.getString("Nome") + " " + resultSet.getString("Cognome"));
-            }
-        } catch (Exception e) {
-            throw new DAOException(e);
+        // Implementazione della logica di aggiornamento specifica
+    }
+
+    public void insertFirma(String dataFirma, int idGiocatore, int idContratto) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(Queries.INSERT_FIRMA)) {
+            statement.setString(1, dataFirma);
+            statement.setInt(2, idGiocatore);
+            statement.setInt(3, idContratto);
+            statement.executeUpdate();
         }
+    }
+
+    public ResultSet getFirmePerGiocatore(int idGiocatore) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(Queries.SELECT_FIRME_PER_GIOCATORE);
+        statement.setInt(1, idGiocatore);
+        return statement.executeQuery();
+    }
+
+    public void updateDataFirma(String dataFirma, int idGiocatore, int idContratto) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(Queries.UPDATE_DATA_FIRMA)) {
+            statement.setString(1, dataFirma);
+            statement.setInt(2, idGiocatore);
+            statement.setInt(3, idContratto);
+            statement.executeUpdate();
+        }
+    }
+
+    public void insertScambio(int idGiocatoreA, int idGiocatoreB, int idSquadraA, int idSquadraB, String data, String dettagli, String stato) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(Queries.INSERT_SCAMBIO)) {
+            statement.setInt(1, idGiocatoreA);
+            statement.setInt(2, idGiocatoreB);
+            statement.setInt(3, idSquadraA);
+            statement.setInt(4, idSquadraB);
+            statement.setString(5, data);
+            statement.setString(6, dettagli);
+            statement.setString(7, stato);
+            statement.executeUpdate();
+        }
+    }
+
+    public ResultSet getScambi(int idSquadraA, int idSquadraB) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(Queries.SELECT_SCAMBI);
+        statement.setInt(1, idSquadraA);
+        statement.setInt(2, idSquadraB);
+        return statement.executeQuery();
     }
 }
